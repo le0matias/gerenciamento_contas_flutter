5f3550e7b7f682e767ec102aa15275e527475696
le0matias <leonardomfrancisco@gmail.com>
2023-01-14 16:34:07 -0300

    feat: add environment variables

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+DEBUG=True
+
+USERNAME='root'
+PASSWORD='chaves123'
+SERVER='localhost'
+DB='contas_flutter'
+
+# SQLALCHEMY_DATABASE_URI=f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
+
+SQLALCHEMY_DATABASE_URI=os.environ['SQLALCHEMY_DATABASE_URI']
+
+SQLALCHEMY_TRACK_MODIFICATIONS=True
+
+SECRET_KEY='chave_secreta1'
---
+++ environment.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:0gPte5pMwDMFwbJmCzH5@containers-us-west-127.railway.app:6836/railway'
